0677d4d81e5314ad31594c33be66b8bb666ad20f
Ayana Griffin <ayanajg@gmail.com>
2025-03-10 22:03:08 -0700

    minor text fix

--- assets/avatarInfo.js
+++ assets/avatarInfo.js
@@ -1,9 +1,9 @@
 /* Description of the avatar for the choices page */
 export const introDesc = {
-  "Positive Percy":
-    "Will support you with kind words and gentle encouragement, making sure you feel safe and valued throughout your sessions!",
-  "Sassy Mary":
-    "Will keep a positive mindset throughout your sessions and motivate you to keep going!",
   "Gentle Joey":
+    "Will support you with kind words and gentle encouragement, making sure you feel safe and valued throughout your sessions!",
+  "Positive Percy":
+    "Will keep a positive mindset throughout your sessions and motivate you to keep going!",
+  "Sassy Mary":
     "Will push you to do your best with a little bit of tough love and a whole lot of sass!",
 };
