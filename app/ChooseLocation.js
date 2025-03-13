49e80a11343778b1bf00135af70cbdc492f3c14b
Ayana Griffin <69114559+ayanagriffin@users.noreply.github.com>
2025-03-13 12:26:50 -0700

    Revert "Lauren"

--- app/ChooseLocation.js
+++ app/ChooseLocation.js
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import { Text, Alert, Image, StyleSheet, View, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useNavigation } from "@react-navigation/native";
-import AsyncStorage from "@react-native-async-storage/async-storage"; 
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage
 import Button from "@/components/Button";
 import Theme from "@/assets/theme";
 import { avatarImages, bgImages } from "@/assets/imgPaths";
-import FontAwesome6 from "@expo/vector-icons/FontAwesome6"; // back icon
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6"; // Import FontAwesome for back icon
 
 export default function ChooseLocation() {
   const [avatarName, setAvatarName] = useState("Loading...");
@@ -191,11 +191,11 @@ const styles = StyleSheet.create({
   avatarImg: {
     height: 270,
     resizeMode: "contain",
-    position: "absolute", 
+    position: "absolute", // Places avatar over background
   },
   backgroundImg: {
-    width: "90%", 
-    height: "100%",
+    width: "90%", // Adjust width for better fit
+    height: "100%", // Covers the allocated space
     resizeMode: "cover",
     borderRadius: 20,
   },
--- app/WorkSession.js
+++ app/WorkSession.js
@@ -247,6 +247,9 @@ export default function WorkSession({ sessionDuration, avatarName, onSessionEnd,
 
                 {/* Controls */}
                 <View style={styles.controls}>
+                    <TouchableOpacity style={styles.controlButton}>
+                        <FontAwesome6 name="forward-fast" size={20} color="black" />
+                    </TouchableOpacity>
                     <TouchableOpacity
                         style={styles.controlButton}
                         onPress={timerPaused ? handlePauseModalResume : () => { handlePause(); setShowPauseModal(true); }}
@@ -365,7 +368,7 @@ const styles = StyleSheet.create({
         bottom: 30,
         flexDirection: "row",
         justifyContent: "space-around",
-        width: "30%",
+        width: "60%",
     },
     controlButton: {
         width: 50,
@@ -434,7 +437,6 @@ const styles = StyleSheet.create({
         height: 400,
         width: 400,
         position: "absolute",
-        marginLeft: 1,
         marginTop: "75%",
     },
     breakModalOverlay: {
